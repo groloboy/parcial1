aa02d68f0ab91cb7c8b3431ed672c1ba07eff07d
Brayan Andrés Quiñonez Ramirez <bquinones@opalo.com.co>
2020-09-15 23:27:37 -0500

    enable fix

--- web2/web2.sh
+++ web2/web2.sh
@@ -10,7 +10,7 @@ lxc launch ubuntu:20.04 web2Back --target web2
 sudo apt-get update
 
 lxc exec web2 -- sudo apt-get install apache2 -y
-lxc exec web2 -- sudo systemctl enable apache2 -y
+lxc exec web2 -- sudo systemctl enable apache2 
 lxc file push /vagrant/web2/web2.html web2/var/www/html/index.html
 lxc exec web2 -- sudo systemctl restart apache2
 lxc config device add web2 myport80 proxy listen=tcp:192.168.100.12:80 connect=tcp:127.0.0.1:80
@@ -18,7 +18,7 @@ lxc config set web2 limits.memory 64MB
 echo "------------web2 Configurada------------"
 
 lxc exec web2Back -- sudo apt-get install apache2 -y
-lxc exec web2Back -- sudo systemctl enable apache2 -y
+lxc exec web2Back -- sudo systemctl enable apache2 
 lxc file push /vagrant/web2/web2Back.html web2Back/var/www/html/index.html
 lxc exec web2Back -- sudo systemctl restart apache2
 lxc config device add web2Back myport5080 proxy listen=tcp:192.168.100.12:5080 connect=tcp:127.0.0.1:80
